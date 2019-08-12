961306b6be9d2702d3c43fe5b0ce6a359b593ff0
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-08-12 16:46:09 -0600

    Turn partially completed

---
+++ test/Turn-test.js
@@ -0,0 +1,42 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card')
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  }); 
+
+  it('should take a player\'s guess', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+    
+    expect(turn.guess).to.equal('pug');
+    expect(turn.currentCard).to.equal(card)
+  });
+
+  it('should return a player\'s guess', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+
+    expect(turn.returnGuess()).to.equal('pug');
+  });
+
+  it('should return the current card', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+
+    expect(turn.returnCard()).to.equal(card);
+  });
+
+  
+});
